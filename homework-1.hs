a00ab6b6f9968385c00095b2765bda96ea512f31
ysgard <ysgard@gmail.com>
2014-11-24 14:01:36 -0800

    Adding alternative hanoi solution

--- homework-1.hs
+++ homework-1.hs
@@ -33,6 +33,11 @@ type Peg = String
 type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 1 a b _ = [(a, b)]
-hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a 
+hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
+
+-- alternative from #haskell-beginners
+hanoi' :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi' 0 _ _ _ = []
+hanoi' n a b c = hanoi' (n-1) a b c ++ (a, b) : hanoi' (n-1) c b a
 
                 
