8ca279043c0c587b06e7b65113acf8c6adf8ac33
ysgard <ysgard@gmail.com>
2014-11-24 13:11:47 -0800

    Finished hanoi example

--- homework-1.hs
+++ homework-1.hs
@@ -34,3 +34,5 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 1 a b _ = [(a, b)]
 hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a 
+
+                
