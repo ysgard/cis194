4b20910825956c27d0723ef71479b0c84682bf52
ysgard <ysgard@gmail.com>
2014-11-26 09:25:51 -0800

    Fixed so that no warnings are reported

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -1,17 +1,18 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 
--- Homework 2 (LogAnalysis)
-
 import Log
+
+
+-- Homework 2 (LogAnalysis)
 
 -- Exercise 1
 -- ex: parseMessage "E 2 562 help help" --> LogMessage (Error 2) 562 "help help"
 parseMessage :: String -> LogMessage
-parseMessage s = case (words s) of
-  ("E":sv:t:msg) -> LogMessage (Error (read sv :: Int)) (read t :: Int) (unwords msg)
-  ("I":t:msg)   -> LogMessage Info (read t :: Int) (unwords msg)
-  ("W":t:msg)   -> LogMessage Warning (read t :: Int) (unwords msg)
+parseMessage s = case words s of
+  ("E" : sv : t : msg) -> LogMessage (Error (read sv :: Int)) (read t :: Int) (unwords msg)
+  ("I" : t : msg)   -> LogMessage Info (read t :: Int) (unwords msg)
+  ("W" : t : msg)   -> LogMessage Warning (read t :: Int) (unwords msg)
   m             -> Unknown (unwords m)
 
 parse :: String -> [LogMessage]
@@ -24,12 +25,14 @@ insert msg Leaf = Node Leaf msg Leaf
 insert msg@(LogMessage _ msgTime _) (Node ltree l@(LogMessage _ treeTime _) rtree)
   | msgTime <= treeTime = Node (insert msg ltree) l rtree 
   | msgTime > treeTime = Node ltree l (insert msg rtree)
+-- Catch all other cases (which shouldn't occur)
+insert _ _ = error "Should never occur!"
                           
 -- Exercise 3
 build :: [LogMessage] -> MessageTree
 build [] = Leaf
 build [x] = Node Leaf x Leaf
-build (x:xs) = insert x (build xs)
+build (x : xs) = insert x (build xs)
 
 -- Exercise 4
 inOrder :: MessageTree -> [LogMessage]
@@ -41,10 +44,13 @@ whatWentWrong :: [LogMessage] -> [String]
 whatWentWrong [] = []
 whatWentWrong logs = toStrings . inOrder . build $ f logs
   where
+    f :: [LogMessage] -> [LogMessage]
     f [] = []
     f (x@(LogMessage (Error sv) _ _):xs)
-      | sv > 50 = x:(f xs)
+      | sv > 50 = x : f xs
       | otherwise = f xs
-    f (x:xs) = f xs
+    f (_ : xs) = f xs
+    toStrings :: [LogMessage] -> [String]
     toStrings [] = []
-    toStrings ((LogMessage _ _ msg):xs) = msg:(toStrings xs)
+    toStrings (LogMessage _ _ msg : xs) = msg : toStrings xs
+    toStrings (Unknown _ : _) = []
