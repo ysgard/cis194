0830b6f356b86c54479cee9e81b01537bd8d750c
ysgard <ysgard@gmail.com>
2014-12-09 08:03:56 -0800

    Fresh sync

---
+++ Homework4/hw4.hs
@@ -0,0 +1,35 @@
+-- CIS 194 (Spring 2013) Homework 4
+module Cis194H4 where
+
+-- Exercise 1
+
+fun1 :: [Integer] -> Integer
+fun1 []       = 1
+fun1 (x:xs)
+  | even x    = (x -2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' xs = foldl (\a b -> (b - 2) * a) 1 $ filter (even) xs
+
+-- Exercise 2
+
+fun2 :: Integer -> Integer
+fun2 1             = 0
+fun2 n | even n    = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+-- Derived from https://github.com/sunchao/upenn-cis194/blob/master/assignments/4/hw4.hs,
+-- Because I was too stupid to figure it out.
+fun2' :: Integer -> Integer
+fun2' n = sum . filter even . takeWhile (/=1) $ iterate (\x -> if even x then x `div` 2 else 3 * x + 1) n
+
+
+-- Exercise 3
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+            deriving (Show, Eq)
+
+                     
+foldTree :: [a] -> Tree a
+foldTree xs = foldr 
--- higher-order.hs
+++ higher-order.hs
@@ -65,5 +65,91 @@ fn' x y = 2 * x + y
 -- Because function application is left-associative, we can omit the parens
 -- (f 3) 2 === f 3 2
 
+-- multi-argument lambda abstraction
+-- \x y z -> ...
+-- is really just syntactic sugar for
+-- \x -> (\y -> (\z -> ...))
+-- and f x y z = ... is really just
+-- f = \x -> (\y -> (\z -> ...))
 
+-- Can thus rewrite the composition function to
+comp :: (b -> c) -> (a -> b) -> a -> c
+comp f g x = f (g x)
 
+-- To actually represent a function that takes two arguments
+-- and not curry them, use a tuple:
+f'' :: (Int, Int) -> Int
+f'' (x, y) = 2 * x + y
+-- Not really, it's still just one argument - it just happens to be a pair
+
+-- to convert between the two representations of a two-argument function
+-- use curry and uncurry - defined like this:
+curry' :: ((a, b) -> c) -> a -> b -> c
+curry' f x y = f (x, y)
+
+uncurry' :: (a -> b -> c) -> (a, b) -> c
+uncurry' f (x, y) = f x y
+
+-- example
+five = uncurry (+) (2,3)
+    
+-- Partial Application
+
+-- Because Haskell doesn't make it escpecially convenient to partially apply
+-- multiple operators (exception: infix operators) it's a bit of an art
+-- figuring out the most useful ordering of the arguments to a function.
+
+-- in practice, use "least ot greatest variation", i.e. the argument that
+-- changes the least should come first, followed by arguments that are
+-- more likely to change and ending with the most variable arguments last.
+
+-- Examples. Whee!
+
+foobar :: [Integer] -> Integer
+foobar []     = 0
+foobar (x:xs)
+  | x > 3     = (7 * x + 2) + foobar xs
+  | otherwise = foobar xs
+
+-- ^ is not good Haskell style.  Does too much at once, works at too low a level
+-- more idiomatic:
+foobar' :: [Integer] -> Integer
+foobar' = sum . map (\x -> 7*x + 2) . filter (>3)
+
+-- the style of coding in which we define a function without referring to its arguments
+-- saying what a function is rather than what it does
+-- is called "point-free" style
+
+-- Folds
+
+sum' :: [Integer] -> Integer
+sum' []         = 0
+sum' (x:xs)     = x + sum' xs
+
+product' :: [Integer] -> Integer
+product' []     = 1
+product' (x:xs) = x * product' xs
+
+length' :: [a] -> Int
+length' []      = 0
+length' (_:xs)  = 1 + length' xs
+
+-- All of these condense lists down to a single value.  In other words, [a] -> a
+fold' :: b -> (a -> b -> b) -> [a] -> b
+fold' z _ []    = z
+fold' z f (x:xs) = f x (fold' z f xs)
+
+-- fold' essentially replaces [] with z and (:) with f, so...
+-- fold' f z [a, b, c] == a `f` (b `f` (c `f` z))
+
+-- rewriting sum', product' and length' in terms of fold
+sum'' = fold' 0 (+)
+product'' = fold' 1 (*)
+length'' = fold' 0 (\_ s -> 1 + s)  -- could also write (\_ -> (1+))
+
+-- foldr means 'fold from the right'
+-- foldl - fold from the left
+-- foldr f z [a,b,c] == a `f` (b `f` (c `f` x))
+-- foldl f z [a,b,c] == ((z `f` a) `f` b) `f` c
+
+-- Should use foldl' from Data.List though, it's more efficient.
